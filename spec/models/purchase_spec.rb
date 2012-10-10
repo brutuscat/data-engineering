6fbdace5cc4dfd5741192829a084ec40102402a7
Mauro Asprea <mauroasprea@gmail.com>
2012-10-10 11:09:46 +0200

    Fix specs

--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -11,30 +11,32 @@ describe Purchase do
   context "import" do
     let(:data){ ["purchaser_name", "item_description", "1.0", 2, "merchant_address", "merchant_name"] }
 
+    it "a purchase" do
+      expect{ Purchase.import(*data) }.to change{Purchase.count}.by(1)
+    end
     it "a purchaser" do
-      expect{ Purchase.import(*data) }.to change{Purchaser.count}.from(0).to(1)
+      expect{ Purchase.import(*data) }.to change{Purchaser.count}.by(1)
     end
     it "an item" do
-      expect{ Purchase.import(*data) }.to change{Item.count}.from(0).to(1)
+      expect{ Purchase.import(*data) }.to change{Item.count}.by(1)
     end
     it "a merchant" do
-      expect{ Purchase.import(*data) }.to change{Merchant.count}.from(0).to(1)
+      expect{ Purchase.import(*data) }.to change{Merchant.count}.by(1)
     end
     it "items count" do
-      expect{ Purchase.import(*data) }.to change{Purchase.first.items.count rescue 0}.from(0).to(2)
+      expect{ Purchase.import(*data) }.to change{Purchase.first.items.count rescue 0}.by(2)
     end
 
     context "finds" do
-      before(:all){
-        FactoryGirl.create(:purchaser, :name => "purchaser_name")
-        FactoryGirl.create(:merchant, :address => "merchant_address", :name => "merchant_name")
-      }
+      # Notice the let!
+      let!(:purchaser){ FactoryGirl.create(:purchaser, :name => "purchaser_name") }
+      let!(:merchant){ FactoryGirl.create(:merchant, :address => "merchant_address", :name => "merchant_name") }
 
       it "a purchaser" do
-        expect{ Purchase.import(*data) }.to_not change{Purchaser.count}.by(1)
+        expect{ Purchase.import(*data) }.to change{Purchase.first.purchaser rescue nil}.from(nil).to(purchaser)
       end
       it "a merchant" do
-        expect{ Purchase.import(*data) }.to_not change{Merchant.count}.by(1)
+        expect{ Purchase.import(*data) }.to change{Purchase.first.merchant rescue nil}.from(nil).to(merchant)
       end
     end
   end
